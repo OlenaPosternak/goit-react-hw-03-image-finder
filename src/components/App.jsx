73d8ee452fc0b5301f7ec66211ee8e1b5dc58d49
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-10-23 22:12:24 +0300

    json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
 import { AppStyled } from './App.module';
-// import axios from 'axios';
+import axios from 'axios';
 import { Component } from 'react';
-// import {REACT_APP_API_KEY} from '../API_KEY'
+import {REACT_APP_API_KEY} from '../API_KEY'
 import { SearchBar } from './SearchBar/SearchBar';
 
-// axios.defaults.baseURL = `https://pixabay.com/api/?q=cat&page=1&key=${REACT_APP_API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+axios.defaults.baseURL = `https://pixabay.com/api/?q=cat&page=1&key=${REACT_APP_API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
 export class App extends Component {
-//   async componentDidMount() {
-//     try {
-//       const response = await axios.get('/flowers');
-//       console.log(response.data.hits);
+  async componentDidMount() {
+    try {
+      const response = await axios.get('/flowers');
+      console.log(response.data.hits);
 
       
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   render() {
     return <AppStyled>
